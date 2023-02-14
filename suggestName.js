d478151ad56595080d852e998fe2d0c045054647
Imad Roula <roulaimad7@gmail.com>
2023-02-14 01:26:09 +0100

    #commmmit

--- suggestName.js
+++ suggestName.js
@@ -72,7 +72,6 @@ function submitfun() {
                                 })
                         }
                     }
-                    
                 } else {
                     fetch(url,
                         {
